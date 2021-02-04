1cfd08043342868894dee23412077559febc5578
uo258538 <uo258538@uniovi.es>
2021-02-04 16:40:00 +0100

    Prac2-1

---
+++ sesion2/webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    mode: "production",
+    entry: {
+         "prac2-1": './src/prac2-1.js',
+         "prac2-2": './src/prac2-2.js',
+    },
+    devServer: {
+        writeToDisk: true
+    }
+};
